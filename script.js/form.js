12e5994b08b7d3649f41a9343c5fe6bf3958fa9c
mdmarufhossain1103-ship-it <mdmarufhossain1103@gmail.com>
2026-03-09 10:41:16 +0600

    connected to form.js

--- script.js/form.js
+++ script.js/form.js
@@ -1,12 +1,12 @@
-// const nameBtn=document.getElementById("name-btn");
-// const passwordBtn=document.getElementById("password-btn");
-// document.getElementById("input-btn").addEventListener("click",()=>{
-//     if(nameBtn.value==="admin" && passwordBtn.value==="admin123"){
-//         window.location.href="next.html";
-//     }
-//     else{
-//         nameBtn.value='';
-//         passwordBtn.value='';
-//         return;
-//     }
-// });
+const nameBtn=document.getElementById("name-btn");
+const passwordBtn=document.getElementById("password-btn");
+document.getElementById("input-btn").addEventListener("click",()=>{
+    if(nameBtn.value==="admin" && passwordBtn.value==="admin123"){
+        window.location.href="next.html";
+    }
+    else{
+        nameBtn.value='';
+        passwordBtn.value='';
+        return;
+    }
+});
