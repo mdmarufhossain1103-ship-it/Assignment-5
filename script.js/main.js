065b57805e934252e8601178e259c2f34401c42d
mdmarufhossain1103-ship-it <mdmarufhossain1103@gmail.com>
2026-03-09 11:13:09 +0600

    add loadcard and diplayloadcard

--- script.js/main.js
+++ script.js/main.js
@@ -7,21 +7,21 @@
 //   }
 // }
 
-// const loadCard=()=>{
-//     const spnnier=document.getElementById("loading-spinner")
-//     const nextCard=document.getElementById("load-card");
-//     spnnier.classList.remove("hidden");
-//     nextCard.classList.add("hidden");
-//     fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
-//     .then(res=>res.json())
-//     .then(data=>{
-//       allData=data.data;
-//       displayCard(allData);
-//       issueCnt(allData.length);
-//       spnnier.classList.add("hidden");
-//       nextCard.classList.remove("hidden");
-//     });
-// };
+const loadCard=()=>{
+    // const spnnier=document.getElementById("loading-spinner")
+    // const nextCard=document.getElementById("load-card");
+    // spnnier.classList.remove("hidden");
+    // nextCard.classList.add("hidden");
+    fetch("https://phi-lab-server.vercel.app/api/v1/lab/issues")
+    .then(res=>res.json())
+    .then(data=>{
+      allData=data.data;
+      displayCard(allData);
+      issueCnt(allData.length);
+    //   spnnier.classList.add("hidden");
+    //   nextCard.classList.remove("hidden");
+    });
+};
 
 // const loadIssueDetail=(id)=>{
 //   fetch(`https://phi-lab-server.vercel.app/api/v1/lab/issue/${id}`)
@@ -71,49 +71,49 @@
 //   document.getElementById("my_modal_1").showModal();
 // }
 
-// const displayCard=(cards)=>{
-//     const nextCard=document.getElementById("load-card");
-//     nextCard.innerHTML="";
-//     cards.forEach(card => {
-//         const newCard=document.createElement("div");
-//         const lebels=card.labels.map(label=>`
-//           <div class="flex items-center gap-1 bg-red-200 text-xs px-1 py-2 rounded-full">${label} </div>
-//           `).join('');
-//          let levelColor="";
-//          if(card.priority==="high"){
-//           levelColor="bg-red-200 text-red-500"
-//          }else if (card.priority==="medium"){
-//           levelColor="bg-yellow-200 text-yellow-500"
-//          }
-//          else if (card.priority==="low"){
-//           levelColor="bg-purple-200 text-purple-500"
-//          }
+const displayCard=(cards)=>{
+    const nextCard=document.getElementById("load-card");
+    nextCard.innerHTML="";
+    cards.forEach(card => {
+        const newCard=document.createElement("div");
+        const lebels=card.labels.map(label=>`
+          <div class="flex items-center gap-1 bg-red-200 text-xs px-1 py-2 rounded-full">${label} </div>
+          `).join('');
+         let levelColor="";
+         if(card.priority==="high"){
+          levelColor="bg-red-200 text-red-500"
+         }else if (card.priority==="medium"){
+          levelColor="bg-yellow-200 text-yellow-500"
+         }
+         else if (card.priority==="low"){
+          levelColor="bg-purple-200 text-purple-500"
+         }
 
-//          let topBorder=card.status === "open"?"border-t-green-500": "border-t-purple-500";
-//          let statusIcon=card.status==="open"?"./assets/Open-Status.png":"./assets/Closed- Status .png";
-//         newCard.innerHTML=`
-//          <div class="shadow p-5 rounded-2xl h-full border-t-4 ${topBorder} cursor-pointer" onclick="loadIssueDetail(${card.id})">
-//           <div class="flex justify-between mb-5">
-//           <img src="${statusIcon}" alt="${card.status}">
-//             <p class="font-bold  border rounded-full w-[35%] flex items-center justify-center ${levelColor}">${card.priority}</p>
-//           </div>
-//           <div class="my-3">
-//             <h2 class="text-xl font-bold">${card.title}</h2>
-//           <p class="text-[#64748B]">${card.description}</p>
-//           </div>
-//         <div class="flex gap-5 mb-4">
-//            ${lebels}
-//           </div>
-//           <hr class="border-gray-200 mb-4 ">
-//           <div class="space-y-2">
-//             <p>#${card.id} by ${card.author}</p>
-//             <p>${card.createdAt}</p>
-//           </div>
-//         </div>
-//         `
-//         nextCard.append(newCard)
-//     });
-// };
+         let topBorder=card.status === "open"?"border-t-green-500": "border-t-purple-500";
+         let statusIcon=card.status==="open"?"./assets/Open-Status.png":"./assets/Closed- Status .png";
+        newCard.innerHTML=`
+         <div class="shadow p-5 rounded-2xl h-full border-t-4 ${topBorder} cursor-pointer" onclick="loadIssueDetail(${card.id})">
+          <div class="flex justify-between mb-5">
+          <img src="${statusIcon}" alt="${card.status}">
+            <p class="font-bold  border rounded-full w-[35%] flex items-center justify-center ${levelColor}">${card.priority}</p>
+          </div>
+          <div class="my-3">
+            <h2 class="text-xl font-bold">${card.title}</h2>
+          <p class="text-[#64748B]">${card.description}</p>
+          </div>
+        <div class="flex gap-5 mb-4">
+           ${lebels}
+          </div>
+          <hr class="border-gray-200 mb-4 ">
+          <div class="space-y-2">
+            <p>#${card.id} by ${card.author}</p>
+            <p>${card.createdAt}</p>
+          </div>
+        </div>
+        `
+        nextCard.append(newCard)
+    });
+};
 
 
 // const filterData=(status,event)=>{
@@ -138,4 +138,4 @@
 //   displayCard(searchData);
 // });
 
-// loadCard();
+loadCard();
